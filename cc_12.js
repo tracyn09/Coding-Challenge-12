09f42522d4d342b8d355d928344048ca8c301732
tracyn09 <tracyn0099@gmail.com>
2025-03-02 08:35:25 -0500

    Task 2–Updated Metric Cards via Array Conversion

--- cc_12.js
+++ cc_12.js
@@ -15,6 +15,40 @@ revenueCard.appendChild(title)
 revenueCard.appendChild(value)
 dashboard.appendChild(revenueCard)
 
+//Task 2
+let profitCard = document.createElement("div");
+profitCard.setAttribute("class", "metric-card");
+profitCard.setAttribute("id", "profitCard");
 
+let profitTitle = document.createElement("h3");
+profitTitle.textContent = "Profit";                     //Creating Profit Card
+let profitValue = document.createElement("p");
+profitValue.textContent = "$0";
+
+profitCard.appendChild(profitTitle);
+profitCard.appendChild(profitValue);
+dashboard.appendChild(profitCard);
+
+let expensesCard = document.createElement("div");
+expensesCard.setAttribute("class", "metric-card");
+expensesCard.setAttribute("id", "expensesCard");
+
+let expensesTitle = document.createElement("h3");
+expensesTitle.textContent = "Expenses";                 //Creating Expenses Card
+let expensesValue = document.createElement("p");
+expensesValue.textContent = "$0";
+
+expensesCard.appendChild(expensesTitle);
+expensesCard.appendChild(expensesValue);
+dashboard.appendChild(expensesCard);
+
+const myNodeList = document.querySelectorAll('.metric-card')
+const array = Array.from(myNodeList)
+
+array.forEach(card => {
+    const title = card.querySelector("h3")
+    title.textContent = `${title.textContent} - Updated`
+    card.style.backgroundColor = "lightpink"
+})
 
 
